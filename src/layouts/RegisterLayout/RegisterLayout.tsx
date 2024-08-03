91d0b107f8c1411f687f6184a299c607f80fc7ab
huong xinh gai <huong xinh gai>
2024-08-03 23:23:22 +0700

    .

---
+++ src/layouts/RegisterLayout/RegisterLayout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RegisterLayout = () => {
+  return (
+    <div>RegisterLayout</div>
+  )
+}
+
+export default RegisterLayout
---
+++ src/useRouterElement.tsx
@@ -0,0 +1,21 @@
+import { useRoutes } from 'react-router-dom'
+import ProductList from './pages/ProductList'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+export default function useRouterElement() {
+  const router = useRoutes([
+    {
+      path: '/',
+      element: <ProductList />
+    },
+    {
+      path: '/login',
+      element: <Login />
+    },
+    {
+      path: '/',
+      element: <Register />
+    }
+  ])
+}
